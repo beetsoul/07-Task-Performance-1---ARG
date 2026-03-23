34b6b4aa9f62db92b49156a18da7fb6caa12d90d
Jroo <159931577+beetsoul@users.noreply.github.com>
2026-03-23 05:17:32 -0800

    Add files via upload

---
+++ GuessApplicationForm/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace GuessApplicationForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.guessTextBox = new System.Windows.Forms.TextBox();
+            this.guessBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(365, 71);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "c??????r";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // guessTextBox
+            // 
+            this.guessTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.guessTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.guessTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessTextBox.Location = new System.Drawing.Point(39, 83);
+            this.guessTextBox.Name = "guessTextBox";
+            this.guessTextBox.Size = new System.Drawing.Size(312, 22);
+            this.guessTextBox.TabIndex = 1;
+            // 
+            // guessBtn
+            // 
+            this.guessBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.guessBtn.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.guessBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.guessBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessBtn.ForeColor = System.Drawing.Color.White;
+            this.guessBtn.Location = new System.Drawing.Point(131, 112);
+            this.guessBtn.Name = "guessBtn";
+            this.guessBtn.Size = new System.Drawing.Size(125, 31);
+            this.guessBtn.TabIndex = 2;
+            this.guessBtn.Text = "Guess";
+            this.guessBtn.UseVisualStyleBackColor = false;
+            this.guessBtn.Click += new System.EventHandler(this.guessBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(383, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Wrong Guess";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox
+            // 
+            this.listBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(386, 35);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(122, 108);
+            this.listBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(528, 155);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.guessBtn);
+            this.Controls.Add(this.guessTextBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Guess The Word";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox guessTextBox;
+        private System.Windows.Forms.Button guessBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox;
+    }
+}
+
---
+++ GuessApplicationForm/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace GuessApplicationForm
+{
+    public partial class Form1 : Form
+
+    {
+
+        string wordToGuess = "computer";
+        StringBuilder displayWord = new StringBuilder();
+        List<string> wrongGuesses = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void guessBtn_Click(object sender, EventArgs e)
+        {
+
+            string guess = guessTextBox.Text.Trim().ToLower();
+
+            if (string.IsNullOrEmpty(guess))
+            {
+                MessageBox.Show("Enter a letter or word!");
+                return;
+            }
+
+            bool correct = false;
+
+ 
+            if (guess.Length == 1)
+            {
+                for (int i = 0; i < wordToGuess.Length; i++)
+                {
+                    if (wordToGuess[i].ToString() == guess)
+                    {
+                        displayWord[i] = wordToGuess[i]; 
+                        correct = true;
+                    }
+                }
+            }
+            else
+            {
+                if (guess == wordToGuess)
+                {
+                    displayWord.Clear();
+                    displayWord.Append(wordToGuess);
+                    correct = true;
+                }
+            }
+
+            if (correct)
+            {
+                label1.Text = displayWord.ToString();
+
+                if (label1.Text == wordToGuess)
+                {
+                    MessageBox.Show("You guessed the word!");
+                }
+            }
+            else
+            {
+                if (!wrongGuesses.Contains(guess))
+                {
+                    wrongGuesses.Add(guess);
+                    listBox.Items.Add(guess);
+                }
+
+                MessageBox.Show("Wrong guess. Try Again!");
+            }
+
+            guessTextBox.Clear();
+        }
+    }
+}
